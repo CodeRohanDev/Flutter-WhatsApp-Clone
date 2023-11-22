399711b460d1645407757377fbfe2f0e792c78ed
CodeRohanDev <rohankumarinsta@gmail.com>
2023-11-22 22:08:33 +0530

    otp screen done

---
+++ whatsappclone/lib/Screens/otpscreen.dart
@@ -0,0 +1,117 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:pin_code_text_field/pin_code_text_field.dart';
+
+class OTPScreen extends StatefulWidget {
+  const OTPScreen({super.key});
+
+  @override
+  State<OTPScreen> createState() => _OTPScreenState();
+}
+
+class _OTPScreenState extends State<OTPScreen> {
+  TextEditingController otpController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(24, 48, 38, 8),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "Images/whatsapp.png",
+                    height: 120,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Welcome to WhatsApp",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontFamily: "Helvetica-Oblique",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Enter SMS code",
+                    style: TextStyle(
+                        fontFamily: "Helvetica-Oblique", letterSpacing: 1),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: PinCodeTextField(
+                      controller: otpController,
+                      autofocus: true,
+                      hideCharacter: false,
+                      highlight: true,
+                      highlightColor: Colors.blue,
+                      defaultBorderColor: Colors.black,
+                      hasTextBorderColor: Colors.green,
+                      maxLength: 6,
+                      hasError: false,
+                      maskCharacter: "*",
+                      onTextChanged: (text) {
+                        setState(() {
+                          // Handle the OTP as it changes
+                        });
+                      },
+                      onDone: (text) {},
+                      wrapAlignment: WrapAlignment.spaceAround,
+                      pinBoxHeight: 40,
+                      pinBoxWidth: 30,
+                      pinTextStyle: TextStyle(fontSize: 18.0),
+                      pinBoxDecoration:
+                          ProvidedPinBoxDecoration.underlinedPinBoxDecoration,
+                      pinTextAnimatedSwitcherTransition:
+                          ProvidedPinBoxTextAnimation.scalingTransition,
+                      pinTextAnimatedSwitcherDuration:
+                          Duration(milliseconds: 300),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  SizedBox(
+                    width: 200,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green, // Background color
+                      ),
+                      child: Text(
+                        "Done",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "Helvetica-Bold",
+                          letterSpacing: 1,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+    ;
+  }
+}
--- whatsappclone/lib/main.dart
+++ whatsappclone/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:whatsappclone/Screens/SplashScreen.dart';
+import 'package:whatsappclone/Screens/otpscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: SplashScreen(),
+      home: OTPScreen(),
     );
   }
 }
