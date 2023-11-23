ebee7c3a41dcecbf1b97dee243380b728eef70c5
CodeRohanDev <rohankumarinsta@gmail.com>
2023-11-23 21:50:48 +0530

    top section done

--- whatsappclone/lib/Screens/Home.dart
+++ whatsappclone/lib/Screens/Home.dart
@@ -1,9 +1,9 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, file_names, avoid_unnecessary_containers
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, file_names
 
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -14,65 +14,102 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          body: Padding(
-        padding:
-            const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 20),
-        child: Container(
-          child: Container(
-            height: 70,
-            decoration: BoxDecoration(
-              color: Colors.red,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "WhatsApp",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: "Helvetica-Bold",
+        body: Container(
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(color: Colors.green),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      top: 10, left: 20, bottom: 0, right: 20),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "WhatsApp",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: "Helvetica-Bold",
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.camera,
+                                size: 28,
+                              ),
+                              SizedBox(width: 8),
+                              Icon(
+                                Icons.search,
+                                size: 28,
+                              ),
+                              SizedBox(width: 8),
+                              Icon(
+                                Icons.menu_open,
+                                size: 28,
+                              ),
+                            ],
+                          )
+                        ],
                       ),
-                    ),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.camera,
-                          size: 28,
+                      SizedBox(height: 20),
+                      DefaultTabController(
+                        length: 4,
+                        child: Column(
+                          children: [
+                            TabBar(
+                              isScrollable: false,
+                              indicatorSize: TabBarIndicatorSize.label,
+                              indicatorWeight: 2.0, // Adjust as needed
+                              labelPadding:
+                                  EdgeInsets.symmetric(horizontal: 8.0),
+                              tabs: [
+                                Icon(
+                                  Icons.people,
+                                  color: Colors.white,
+                                  size: 28,
+                                ),
+                                Text(
+                                  "Chats",
+                                  style: TextStyle(
+                                      fontFamily: "Helventica-Bold",
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20),
+                                ),
+                                Text(
+                                  "Status",
+                                  style: TextStyle(
+                                      fontFamily: "Helventica-Bold",
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20),
+                                ),
+                                Text(
+                                  "Calls",
+                                  style: TextStyle(
+                                    fontFamily: "Helventica-Bold",
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
-                        SizedBox(width: 8),
-                        Icon(
-                          Icons.search,
-                          size: 28,
-                        ),
-                        SizedBox(width: 8),
-                        Icon(
-                          Icons.menu_open,
-                          size: 28,
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-                DefaultTabController(
-                  length: 4,
-                  child: TabBar(
-                    tabs: [
-                      Icon(Icons.people),
-                      Text("Chats"),
-                      Text("Status"),
-                      Text("Calls"),
+                      ),
                     ],
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-      )),
+      ),
     );
   }
 }
