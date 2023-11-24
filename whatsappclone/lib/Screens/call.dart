9cdaf9326c974ab283cbfc69aeb14d602dc127cf
CodeRohanDev <rohankumarinsta@gmail.com>
2023-11-24 21:43:23 +0530

    calls page template ready

---
+++ whatsappclone/lib/Screens/call.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class CallsPage extends StatefulWidget {
+  const CallsPage({super.key});
+
+  @override
+  State<CallsPage> createState() => _CallsPageState();
+}
+
+class _CallsPageState extends State<CallsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Calls"),
+      ),
+    );
+  }
+}
