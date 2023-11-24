5fa9d553fb917129c34a89156420ad41aaa1140e
CodeRohanDev <rohankumarinsta@gmail.com>
2023-11-24 21:45:04 +0530

    Community template added

---
+++ whatsappclone/lib/Screens/community.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class CommunityPage extends StatefulWidget {
+  const CommunityPage({super.key});
+
+  @override
+  State<CommunityPage> createState() => _CommunityPageState();
+}
+
+class _CommunityPageState extends State<CommunityPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Community"),
+      ),
+    );
+  }
+}
