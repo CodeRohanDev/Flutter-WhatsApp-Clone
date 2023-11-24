e8727eb907296b1bbf772c4f6d360856d4b2b0f9
CodeRohanDev <rohankumarinsta@gmail.com>
2023-11-24 21:44:18 +0530

    chats template added

--- whatsappclone/lib/Screens/chats.dart
+++ whatsappclone/lib/Screens/chats.dart
@@ -1,1 +1,21 @@
+// ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/material.dart';
+
+class ChatsPage extends StatefulWidget {
+  const ChatsPage({super.key});
+
+  @override
+  State<ChatsPage> createState() => _ChatsPageState();
+}
+
+class _ChatsPageState extends State<ChatsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Chats"),
+      ),
+    );
+  }
+}
