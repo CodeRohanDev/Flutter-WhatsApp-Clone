00a0f107a170c72af930946d754136cd93eadf1f
CodeRohanDev <rohankumarinsta@gmail.com>
2023-12-06 20:29:49 +0530

    font family updated

--- whatsappclone/lib/Screens/chats.dart
+++ whatsappclone/lib/Screens/chats.dart
@@ -18,6 +18,7 @@ class _ChatsPageState extends State<ChatsPage> {
           "Chats",
           style: TextStyle(
             fontSize: 20
+            fontFamily: "Helvetica"
           ),
         ),
       ),
